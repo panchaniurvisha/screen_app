da6a8002e5b8eb55be7c21d15939cc5b849bbb55
Panchani Urvihsa <urvisha1109@gmail.com>
2023-04-04 16:20:59 +0530

    4/4/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:screen_app/fifth_screen_.gridview.dart';
-import 'package:screen_app/seven_screen.dart';
+import 'package:screen_app/home_screen.dart';
 //import 'package:screen_app/first_screen.dart';
 //import 'package:screen_app/six_screen.dart';
 
@@ -20,6 +19,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const SevenScreen());
+        home: const HomeScreen());
   }
 }
